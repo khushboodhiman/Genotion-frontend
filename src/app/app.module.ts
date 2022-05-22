1e9920debaaade924910f691677861673509c545
khushboodhiman <khushboodhiman716@gmail.com>
2022-05-22 18:15:09 +0530

    Login page frontend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { Login } from 'src/login/login.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Login
   ],
   imports: [
     BrowserModule,
---
+++ src/login/login.module.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'login-form',
+  templateUrl: './login.module.html',
+  styleUrls: ['login.module.css']
+})
+
+export class Login {
+
+}
